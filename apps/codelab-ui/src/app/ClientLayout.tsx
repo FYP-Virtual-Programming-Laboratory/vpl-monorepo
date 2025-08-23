70186a4980c0ad89f9daedee8a73eb0d148009f4
Joshua Farayola <farayolajoshua@gmail.com>
2025-08-24 00:39:18 +0100

    feat: set up @tanstack/react-query and devtools
    Include @tanstack/react-query for data fetching and
    @tanstack/react-query-devtools for enhanced debugging.

--- apps/codelab-ui/src/app/ClientLayout.tsx
+++ apps/codelab-ui/src/app/ClientLayout.tsx
@@ -1,27 +1,38 @@
-"use client"
+"use client";
 
-import type React from "react"
-import "@/app/globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/toaster"
+import "@/app/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import queryClient from "@/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type React from "react";
 
 export default function ClientLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen bg-gray-100">
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-          <DynamicLayout>{children}</DynamicLayout>
-          <Toaster />
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <DynamicLayout>{children}</DynamicLayout>
+            <Toaster />
+          </ThemeProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </body>
     </html>
-  )
+  );
 }
 
 function DynamicLayout({ children }: { children: React.ReactNode }) {
-  return <div className="min-h-screen">{children}</div>
+  return <div className="min-h-screen">{children}</div>;
 }
